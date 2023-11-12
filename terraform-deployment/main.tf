405fcb32117cf0bc2f85295646e9186cd9099272
ch0ufleur <jimmy.bell@polymtl.ca>
2023-11-12 18:44:27 -0500

    Removed placeholder vpn and extra lb

--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -1,52 +1,44 @@
 provider "aws" {
-  region = "ca-central-1"  # Set your desired AWS region
+  region = "ca-central-1"  # Canada Central Region Deployment
 }
 
-resource "aws_launch_configuration" "example" {
-  name = "example_config"
-  image_id = "ami-12345678"  # Specify the AMI ID of your VM image
-  instance_type = "t2.micro"  # Specify the instance type
+resource "aws_launch_configuration" "commander_scaler_config" {
+  name = "commander_scaler_config"
+  image_id = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 22.04 LTS x86_64
+  instance_type = "t2.micro" # Limitation
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello from the user data script!" > /tmp/user_data_output.txt
-              # Add your custom script here or provide a script file URL
+              echo "Hello world!" > /tmp/user_data_output.txt
               EOF
 }
 
-resource "aws_autoscaling_group" "example" {
+resource "aws_autoscaling_group" "commander_scaler_config_group" {
+  // These values are here for our project's demonstration purposes. In a real setting they could be augmented.
   desired_capacity     = 2
   max_size             = 4
   min_size             = 1
-  launch_configuration = aws_launch_configuration.example.id
+  launch_configuration = aws_launch_configuration.commander_scaler_config.id
 
-  tag {
-    key                 = "Name"
-    value               = "example-instance"
-    propagate_at_launch = true
-  }
+//  tag {
+//    key                 = "C"
+//    value               = "example-instance"
+//    propagate_at_launch = true
+//  }
 
   health_check_type          = "EC2"
   health_check_grace_period  = 300
 }
 
-resource "aws_lb" "external" {
+resource "aws_lb" "c2_lb" {
   name               = "external-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["sg-0123456789abcdef0"]  # Specify your security group IDs
-  subnets            = ["subnet-0123456789abcdef0", "subnet-0123456789abcdef1"]  # Specify your subnet IDs
+  security_groups    = [""]  # Specify your security group IDs
+  subnets            = [""]  # Specify your subnet IDs
 }
 
-resource "aws_lb" "internal" {
-  name               = "internal-lb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = ["sg-0123456789abcdef1"]  # Specify your security group IDs
-  subnets            = ["subnet-0123456789abcdef2", "subnet-0123456789abcdef3"]  # Specify your subnet IDs
-}
-
-resource "aws_lb_listener" "external" {
+resource "aws_lb_listener" "c2_lb" {
   load_balancer_arn = aws_lb.external.arn
   port              = 80
   protocol          = "HTTP"
@@ -63,56 +55,27 @@ resource "aws_lb_listener" "external" {
   }
 }
 
-resource "aws_lb_listener" "internal" {
-  load_balancer_arn = aws_lb.internal.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.internal.arn
-    type             = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "OK"
-      status_code  = "200"
-    }
-  }
-}
-
-resource "aws_lb_target_group" "external" {
+resource "aws_lb_target_group" "c2_lb" {
   name     = "external-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "vpc-0123456789abcdef0"  # Specify your VPC ID
 }
 
-resource "aws_lb_target_group" "internal" {
-  name     = "internal-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-0123456789abcdef0"  # Specify your VPC ID
-}
 
-resource "aws_lb_target_group_attachment" "external" {
+resource "aws_lb_target_group_attachment" "c2_lb" {
   target_group_arn = aws_lb_target_group.external.arn
-  target_id        = aws_autoscaling_group.example.id
-  port             = 80
-}
-
-resource "aws_lb_target_group_attachment" "internal" {
-  target_group_arn = aws_lb_target_group.internal.arn
-  target_id        = aws_autoscaling_group.example.id
+  target_id        = aws_autoscaling_group.c2_lb.id
   port             = 80
 }
 
 # Adding GuardDuty
-resource "aws_guardduty_detector" "example" {
+resource "aws_guardduty_detector" "c2_detector" {
   enable = true
 }
 
 # Adding CloudWatch Alarms (Example: CPU Utilization Alarm)
-resource "aws_cloudwatch_metric_alarm" "example" {
+resource "aws_cloudwatch_metric_alarm" "c2_alarm" {
   alarm_name          = "example-cpu-utilization-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
