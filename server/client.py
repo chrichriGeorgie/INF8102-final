100cf0586d9e47bef59b38332a7784e0393c82b6
munozcel <60045589+munozcel@users.noreply.github.com>
2023-11-30 16:15:30 -0500

    HTTPS Server

--- server/client.py
+++ server/client.py
@@ -1,7 +1,18 @@
 import requests
+import os
+print("Current directory:", os.getcwd())
 
-url = 'http://localhost:8000'
-data = {'message': 'Hello, World'}
-response = requests.post(url, json=data)
+# Using HTTPS URL
+url = 'https://localhost:8000'
+data = {'message': 'Hello, HTTPS FINAL TEST 111'}
 
-print(response.text)
+# Correct the path to the certificate if necessary
+cert_path = './8102cert.pem'  # Update this path if necessary
+
+try:
+    response = requests.post(url, json=data, verify=cert_path)
+    print(response.text)
+except requests.exceptions.SSLError as e:
+    print(f"SSL Error: {e}")
+except Exception as e:
+    print(f"Error: {e}")
