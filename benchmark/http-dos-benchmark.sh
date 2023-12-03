98072ab74d6c312967bc330bc4b079d1adbb7b17
ch0ufleur <jimmy.bell@polymtl.ca>
2023-12-03 14:48:25 -0500

    fix benchmark http

--- benchmark/http-dos-benchmark.sh
+++ benchmark/http-dos-benchmark.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Define the number of requests and the time limit
+
 requests=1000
 time_limit=30
+threads=10
 
-# Define the number of threads to use
-threads=1
 
-# Define the URL to request
-url="https://euybn4hu7o43xdp67ykmctr2wa0qajtb.lambda-url.us-east-1.on.aws/"
-
-# Define the function to execute in parallel
 function request() {
-    curl -v "$url" > /dev/null
+    curl -v https://euybn4hu7o43xdp67ykmctr2wa0qajtb.lambda-url.us-east-1.on.aws/
 }
 
 export -f request
 
-# Execute the requests in parallel
+# Executes the requests in parallel
 seq $requests | xargs -n1 -P$threads -I{} bash -c 'request'
 
-# Wait for the time limit to expire
 sleep $time_limit
 
